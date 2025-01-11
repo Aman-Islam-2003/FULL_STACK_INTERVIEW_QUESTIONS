04aef3105c1cf4f0422a96fa23373ea1ace676d5
Aman Islam <amanislam019@gmail.com>
2025-01-11 23:20:46 +0500

    Create useMemo.js

---
+++ react/useMemo.js
@@ -0,0 +1,18 @@
+// useMemo and useCallback both are used for performance optimization in react
+
+//use memo retains the result in cache
+//usecallback stores the entire ftn in cache
+
+function Memo(props){
+
+    const [counterOne, setCounterOne] = useState(0);
+    const [counterTwo, setCounterTwo ] = useState(0);
+    return(
+        <div>
+            
+
+        </div>
+    )
+
+}
+
