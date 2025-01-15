af839890148b4019064297601c4af8578c35dd79
Aman Islam <amanislam019@gmail.com>
2025-01-15 23:17:34 +0500

    basic boilerplate

---
+++ react/vite-project/src/components/button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({onClick}) => {
+    console.log("button component")
+  return (
+    <div>
+      <button onClick={onClick}>Click me</button>
+    </div>
+  )
+}
+
+export default React.memo(Button)
