92d47ea6135e7fb69717791e08bb859bb448927f
Aman Islam <amanislam019@gmail.com>
2025-01-15 23:17:41 +0500

    Create App.jsx

---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useCallback } from "react";
+import Button from "./components/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = useCallback(() => {
+    setCount(prev => prev + 1);
+  },[]);
+
+  return <div>
+    <h2>{count}</h2>
+    <Button onClick={incrementCount}/>
+  </div>;
+}
+
+export default App;
