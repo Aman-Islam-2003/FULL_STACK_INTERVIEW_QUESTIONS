12d635c08635ab89d3447331d040b3329a3018cd
Aman Islam <amanislam019@gmail.com>
2025-01-15 23:17:37 +0500

    Update useMemo.js

--- react/useMemo.js
+++ react/useMemo.js
@@ -3,16 +3,5 @@
 //use memo retains the result in cache
 //usecallback stores the entire ftn in cache
 
-function Memo(props){
 
-    const [counterOne, setCounterOne] = useState(0);
-    const [counterTwo, setCounterTwo ] = useState(0);
-    return(
-        <div>
-            
-
-        </div>
-    )
-
-}
 
