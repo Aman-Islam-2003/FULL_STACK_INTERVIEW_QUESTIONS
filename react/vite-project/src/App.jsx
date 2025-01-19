abe2b27659f415974a88412ca7d2569a25d36e0a
Aman Islam <amanislam019@gmail.com>
2025-01-19 23:38:15 +0500

    Update App.jsx

--- react/vite-project/src/App.jsx
+++ react/vite-project/src/App.jsx
@@ -5,13 +5,15 @@ function App() {
   const [count, setCount] = useState(0);
 
   const incrementCount = useCallback(() => {
-    setCount(prev => prev + 1);
-  },[]);
+    setCount((prev) => prev + 1);
+  }, []);
 
-  return <div>
-    <h2>{count}</h2>
-    <Button onClick={incrementCount}/>
-  </div>;
+  return (
+    <div>
+      <h2>{count}</h2>
+      <Button onClick={incrementCount} />
+    </div>
+  );
 }
 
 export default App;
