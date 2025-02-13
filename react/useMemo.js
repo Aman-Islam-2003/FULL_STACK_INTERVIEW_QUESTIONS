26116e011c203d459f13ec79783bd460f077f908
Aman Islam <amanislam019@gmail.com>
2025-02-13 23:43:29 +0500

    Delete useMemo.js

--- react/useMemo.js
+++
@@ -1,7 +0,0 @@
-// useMemo and useCallback both are used for performance optimization in react
-
-//use memo retains the result in cache
-//usecallback stores the entire ftn in cache
-
-
-//React.memo caches the entire component so when it's props or state dosn't change it is not rerendered
